da2f7daba10e57ccffdc6b9b813a01dc3e4eae70
Đổng Giang Thái <donggiangthai1998@gmail.com>
2022-05-29 11:06:24 +0700

    Using fbmessenger library and json file
    Using fbmessenger to handle and process message, json file for auto response data.

--- src/webhook/settings.py
+++ src/webhook/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import json
 from pathlib import Path
 from decouple import config
 
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# init data
+with open("C:\\Users\\thai\\Desktop\\SourceTree\\webhook-django\\data\\data.json") as f:
+    DATA = json.load(f)
